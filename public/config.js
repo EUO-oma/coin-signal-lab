e96e87c23adbb3c9d4ec6a55699e4566e7059c69
사용자 <icandoit13579@gmail.com>
2026-04-28 22:49:29 +0900

    chore: set Supabase public URL and anon key for hosted app

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "__SUPABASE_URL__",
-  SUPABASE_ANON_KEY: "__SUPABASE_ANON_KEY__",
+  SUPABASE_URL: "https://nfhitgsktvgloptaxyqq.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5maGl0Z3NrdHZnbG9wdGF4eXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NTU3MDYsImV4cCI6MjA4OTIzMTcwNn0.byTb64SmbIFsXx_sNt8AX3MXmgwtp1yowCugAKJtWEo",
   TF: "1h"
 };
